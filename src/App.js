71339ce34dd0855ec74ddd26d75bfde015025f7f
Bhumi Patel <bhumip214@gmail.com>
2019-06-02 15:52:18 -0700

    Updated sideNavBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import * as requests from './requests';
 import Header from './components/Header';
 import SideNavBar from './components/SideNavBar';
+import Products from './components/Products';
 
 export default function App() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
   // Here as an example to get you started with requests.js
   React.useEffect(() => {
     (async () => {
-      const categories = await requests.getCategories();
-      const products = await requests.getProducts({
-        categoryId: categories[0].id,
+      const categoriesResponse = await requests.getCategories();
+      setCategories(categoriesResponse);
+      const productsResponse = await requests.getProducts({
+        categoryId: categoriesResponse[0].id,
       });
-      const product = await requests.getProduct(products[0].id);
+      setProducts(productsResponse);
+      const product = await requests.getProduct(productsResponse[0].id);
       console.log('Example request: categories', categories);
       console.log('Example request: products', products);
       console.log('Example request: product', product);
@@ -22,7 +28,8 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      <SideNavBar />
+      <SideNavBar categories={categories} />
+      <Products products={products} />
     </div>
   );
 }
--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
-export default function SideNavBar() {
+export default function SideNavBar(props) {
   return (
     <div className="sideNavBar">
       <h3 className="nav-tittle"> ALL CATEGORIES</h3>
       <div className="list">
-        <ul>
-          <h3> Creams & gels </h3>
-          <h3> Patches </h3>
-          <h3> Emulsions </h3>
-          <h3> Toners </h3>
-          <h3> Face cleansing </h3>
-          <h3> Makeup removal </h3>
-          <h3> Scrubs </h3>
-          <h3> Peeling </h3>
-          <h3> Masks </h3>
-        </ul>
+        {props.categories.map(category => {
+          return <h3> {category.name} </h3>;
+        })}
       </div>
       <h3 className="nav-tittle"> FILTER BY PRICE</h3>
       <div className="filter-price">
